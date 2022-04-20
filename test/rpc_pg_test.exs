49f719513939ea989078760fb20312c2ca1a8b87
Vinnie Franco <vince@freshivore.net>
2022-04-20 16:32:56 -0500

    Adding some tests and code quality tooling

--- test/rpc_pg_test.exs
+++ test/rpc_pg_test.exs
@@ -1,8 +1,53 @@
 defmodule RpcPGTest do
   use ExUnit.Case
+
+  import ExUnit.CaptureLog
+
   doctest RpcPG
 
-  test "greets the world" do
-    assert RpcPG.hello() == :world
+  describe "join_group/2 with role: :client" do
+    test "returns nil" do
+      assert nil == RpcPG.join_group(:anything, role: :client)
+    end
+  end
+
+  describe "join_group/2 with role: :server" do
+    setup do
+      :pg.start_link(RpcPG.pg_scope())
+
+      :ok
+    end
+
+    test "adds when group is empty" do
+      assert [] == :pg.get_local_members(RpcPG.pg_scope(), "unit")
+
+      pid = self()
+
+      {_result, log} =
+        with_log(fn ->
+          RpcPG.join_group("unit", role: :server)
+        end)
+
+      assert [^pid] = :pg.get_local_members(RpcPG.pg_scope(), "unit")
+      assert log =~ ~s(RpcPG.Server:join group: "unit")
+    end
+
+    test "adds at most once" do
+      pid = self()
+
+      {_result, log} =
+        with_log(fn ->
+          RpcPG.join_group("unit", role: :server)
+        end)
+
+      RpcPG.join_group("unit", role: :server)
+
+      assert [^pid] = :pg.get_local_members(RpcPG.pg_scope(), "unit")
+      assert log =~ ~s(RpcPG.Server:join group: "unit")
+    end
+  end
+
+  test "pg_scope/0 returns :rpc_pg" do
+    assert :rpc_pg == RpcPG.pg_scope()
   end
 end
